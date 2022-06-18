444eab71bd96ff05bc8230c2c2b5ccd2e891039a
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2022-06-18 14:32:56 -0500

    init repod, set up deps, created primereact form

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
