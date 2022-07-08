169e1dfe74e2a5d06806505a71d76e5405816afc
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2022-07-07 19:23:08 -0500

    created basictable

---
+++ src/app/components/BasicTable.tsx
@@ -0,0 +1,69 @@
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { MultiSelect, MultiSelectChangeParams } from "primereact/multiselect";
+import { useState } from "react";
+
+interface Props<T> {
+	rows: T[];
+	columns: { field: string; header: string }[];
+	defaultColumns?: { field: string; header: string }[];
+	selected: T | undefined;
+	setSelected: (value: React.SetStateAction<T | undefined>) => void;
+}
+
+const BasicTable = <T,>({
+	rows,
+	columns,
+	defaultColumns = undefined,
+	selected,
+	setSelected
+}: Props<T>) => {
+	const [selectedColumns, setSelectedColumns] = useState(
+		defaultColumns ?? columns
+	);
+
+	const onColumnToggle = (event: MultiSelectChangeParams) => {
+		const cols = event.value as { field: string; header: string }[];
+		const updatedCols = columns.filter((col) =>
+			cols.some((sCol) => sCol.field === col.field)
+		);
+		setSelectedColumns(updatedCols);
+	};
+
+	const renderColumns = () =>
+		selectedColumns.map((col) => (
+			<Column key={`col-${col.field}`} field={col.field} header={col.header} />
+		));
+
+	const header = (
+		<div style={{ textAlign: "left" }}>
+			<MultiSelect
+				value={selectedColumns}
+				options={columns}
+				optionLabel="header"
+				onChange={onColumnToggle}
+				style={{ width: "20em" }}
+			/>
+		</div>
+	);
+
+	return (
+		<>
+			<DataTable
+				value={rows}
+				header={header}
+				selection={selected}
+				selectionMode="single"
+				onSelectionChange={(e) => setSelected(e.value)}
+				size="small"
+				responsiveLayout="scroll"
+				resizableColumns
+				showGridlines
+			>
+				{renderColumns()}
+			</DataTable>
+		</>
+	);
+};
+
+export default BasicTable;
