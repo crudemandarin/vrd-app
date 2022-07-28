bf8d9f9f3b5250fb224d7c20ae113ab2f9bcc24d
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2022-07-28 15:15:27 -0700

    added 404 page and styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,12 +7,14 @@ import Splash from "./splash/splash.page";
 import Dashboard from "./dashboard/dashboard.page";
 import TradeEntry from "./trade/trade-entry.page";
 import TradeSubmission from "./trade/trade-submission.page";
+import Error404 from "./common/components/Error404";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
+					<Route path="*" element={<Error404 />} />
 					<Route path="/" element={<Splash />} />
 					<Route
 						path="dashboard"
--- src/app/common/components/Error404.tsx
+++ src/app/common/components/Error404.tsx
@@ -1,9 +1,14 @@
 const Error404 = () => {
 	return (
-		<main>
-			<h2>404 Page Not Found</h2>
-			<div className="s-1" />
-			<p>This is not the page you're looking for</p>
+		<main
+			className="flex justify-content-center align-items-center"
+			style={{ height: "20vh" }}
+		>
+			<div>
+				<h2>404 Page Not Found</h2>
+				<div className="s-1" />
+				<p>This is not the page you're looking for</p>
+			</div>
 		</main>
 	);
 };
